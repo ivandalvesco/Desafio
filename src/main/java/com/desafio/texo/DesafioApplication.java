ba01064804c7bcae2909bc810d04a47a616a66fc
ivandalvesco <ivan.dal.vesco@gmail.com>
2019-05-01 20:05:58 -0300

    commit alterações execução

--- src/main/java/com/desafio/texo/DesafioApplication.java
+++ src/main/java/com/desafio/texo/DesafioApplication.java
@@ -1,11 +1,13 @@
 package com.desafio.texo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.desafio.texo.config.ConfigRepository;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DesafioApplication {
 
 	public static void main(String[] args) {
